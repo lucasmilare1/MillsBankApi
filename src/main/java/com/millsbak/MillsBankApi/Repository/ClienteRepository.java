d33b9ec22decf991b9b464fc8cd14dbc5c0cf578
lucasmilare1 <65171151+lucasmilare1@users.noreply.github.com>
2021-02-04 13:38:33 -0300

    Update ClienteRepository.java

--- src/main/java/com/millsbak/MillsBankApi/Repository/ClienteRepository.java
+++ src/main/java/com/millsbak/MillsBankApi/Repository/ClienteRepository.java
@@ -1,6 +1,6 @@
 package com.millsbak.MillsBankApi.Repository;
 
-import ch.qos.logback.core.net.server.Client;
+
 import com.millsbak.MillsBankApi.Model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
